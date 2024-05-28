01cfe3ffb4624b1e5a5951efe3940bb8ecd791de
Sam Kikoma <kikomasamson@gmail.com>
2024-05-28 22:44:52 +0200

    State was lifted up to App to make it accessible by other components

--- IP-Add-Tracker/src/App.jsx
+++ IP-Add-Tracker/src/App.jsx
@@ -1,9 +1,25 @@
-import React from 'react'
+import React, { useState } from "react";
+import Search from "./components/Search";
 
 const App = () => {
-  return (
-    <div>App</div>
-  )
-}
+  const [search, setSearch] = useState("");
+  const [response, setResponse] = useState(null);
+  const API_KEY = import.meta.env.VITE_API_KEY;
 
-export default App
+  async function fetchData(e) {
+    e.preventDefault();
+    const url = `https://geo.ipify.org/api/v2/country?apiKey=${API_KEY}&ipAddress=${search}`;
+    const res = await fetch(url);
+    const json = await res.json();
+    setResponse(json);
+    setSearch("");
+  }
+
+  return (
+    <>
+      <Search fetchData={fetchData} search={search} setSearch={setSearch} />
+    </>
+  );
+};
+
+export default App;
