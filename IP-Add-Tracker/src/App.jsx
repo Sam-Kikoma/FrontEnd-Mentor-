e367207d1137cabced51e256fa7b28c7a81d6ba1
Sam Kikoma <kikomasamson@gmail.com>
2024-05-30 20:26:11 +0200

    Changed state management to parent component

--- IP-Add-Tracker/src/App.jsx
+++ IP-Add-Tracker/src/App.jsx
@@ -1,5 +1,7 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import Search from "./components/Search";
+import Results from "./components/Results";
+import Map from "./components/Map";
 
 const App = () => {
   const [search, setSearch] = useState("");
@@ -8,16 +10,29 @@ const App = () => {
 
   async function fetchData(e) {
     e.preventDefault();
-    const url = `https://geo.ipify.org/api/v2/country?apiKey=${API_KEY}&ipAddress=${search}`;
+    const url = `https://geo.ipify.org/api/v2/country,city?apiKey=${API_KEY}&ipAddress=${search}`;
     const res = await fetch(url);
     const json = await res.json();
     setResponse(json);
     setSearch("");
   }
 
+  async function initialFetch() {
+    const url = `https://geo.ipify.org/api/v2/country,city?apiKey=${API_KEY}&ipAddress=`;
+    const res = await fetch(url);
+    const json = await res.json();
+    setResponse(json);
+  }
+
+  useEffect(() => {
+    initialFetch();
+  }, []);
+
   return (
     <>
       <Search fetchData={fetchData} search={search} setSearch={setSearch} />
+      {response ? <Results response={response} /> : null}
+      {response ? <Map response={response} /> : null}
     </>
   );
 };
--- IP-Add-Tracker/src/components/Search.jsx
+++ IP-Add-Tracker/src/components/Search.jsx
@@ -1,12 +1,15 @@
-import React from "react";
+import React, { useEffect } from "react";
 
 const Search = ({ fetchData, search, setSearch }) => {
   const handleChange = (e) => {
     setSearch(e.target.value);
   };
+  useEffect(() => {
+    fetchData();
+  }, [fetchData]);
 
   return (
-    <>
+    <div className="search-wrapper" style={{}}>
       <p>IP Address Tracker</p>
       <form action="" onSubmit={fetchData}>
         <input
@@ -15,11 +18,15 @@ const Search = ({ fetchData, search, setSearch }) => {
           id="search"
           value={search}
           onChange={handleChange}
+          placeholder="Search for any IP address or domain"
         />
-        <button>Search</button>
+        <button>
+          <svg xmlns="http://www.w3.org/2000/svg" width="11" height="14">
+            <path fill="none" stroke="#FFF" strokeWidth="3" d="M2 1l6 6-6 6" />
+          </svg>
+        </button>
       </form>
-      <p>{search}</p>
-    </>
+    </div>
   );
 };
 
