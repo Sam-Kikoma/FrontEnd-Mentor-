74e6db7e4e3d0b592b53d0b45d71840f6ae104e2
Sam Kikoma <kikomasamson@gmail.com>
2024-07-09 00:04:35 +0200

    Finished styling and implemented part of filtering logic

--- JobFilter/src/App.jsx
+++ JobFilter/src/App.jsx
@@ -1,30 +1,52 @@
-import Item from "./components/Item"
-import {useEffect, useState} from "react"
-import axios from "axios"
-
+import Item from "./components/Item";
+import { useEffect, useState } from "react";
+import axios from "axios";
 
 const App = () => {
-  const [data, setData] = useState([])
-  
+  const [data, setData] = useState([]);
+  const [filter, setFilter]  = useState([])
+  const [filteredData, setFilteredData] = useState([])
   const fetchData = async () => {
-    const res = await axios.get("http://localhost:3001/users")
-    setData(res.data)
-    console.log(res.data);
-  }
+    try {
+      const res = await axios.get("http://localhost:3001/users");
+      setData(res.data);
+    } catch (error) {
+      console.error("Error fetching data:", error);
+    }
+  };
+  const handleFilter = (filter) => {
+    setFilter((prevFilters) => {
+      if (!prevFilters.includes(filter)) {
+        return [...prevFilters, filter];
+      }
+      return prevFilters;
+    });
+    const newData = data.filter((item) => {
+      return (
+        (item.languages && item.languages.includes(filter)) || (item.role&&item.role.includes(filter)) || (item.level&&item.level.includes(filter))
+        || (item.tools && item.tools.includes(filter))
+      );
+    });
+  
+    setFilteredData((prevFilteredData) => [...prevFilteredData, ...newData]);
+  };
 
-  useEffect(()=>{
-    fetchData()
-  },[])
+  useEffect(() => {
+    fetchData();
+  }, []);
 
   return (
     <>
-      <div className="w-full h-full">
-        <div className="w-auto h-[156px] bg-[--Primary-Dark-Cyan] bg-[url('./assets/bg-header-mobile.svg')] md:[url('./assets/bg-header-desktop.svg')]"></div>
-        {!data ? null : data.map((item)=>(<Item data={item} key={item.id}/>))}
+      <div className="w-full h-full bg-[--Light-Grayish-Cyan]">
+        <div className="w-auto h-[156px] bg- bg-[--Primary-Dark-Cyan] bg-[url('./assets/bg-header-mobile.svg')] md:bg-[url('./assets/bg-header-desktop.svg')] mb-20"></div>
+        {data.length > 0 ? (
+          data.map((item) => <Item data={item} key={item.id} handleFilter={handleFilter}/>)
+        ) : (
+          <p>No data available</p>
+        )}
       </div>
-
     </>
-  )
-}
+  );
+};
 
-export default App
+export default App;
--- JobFilter/src/components/Item.jsx
+++ JobFilter/src/components/Item.jsx
@@ -1,24 +1,36 @@
 /* eslint-disable react/prop-types */
-const Item = ({data}) => {
+const Item = ({data,handleFilter}) => {
+  
+  const dataSet = [];
+  if (data.languages && data.tools) {
+    dataSet.push(...data.languages, ...data.tools);
+  }
+  const handleClick = (e) => {
+    handleFilter(e.target.innerText)
+    console.log(e.target.innerText);
+  }
+  
   return (
-    <div className="w-[80%] border border-green-300 h-48 rounded-sm">
-        <div>
-            <img src={data.logo} alt="" />
+    <div className="max-w-[80%] border-l-4 border-l-[--Primary-Dark-Cyan] h-auto rounded-sm m-auto mb-12 p-4 flex-col-center md:flex-row-space shadow-lg bg-white">
+        <div className="md:flex md:w-[60%]">
+          <div className="mt-[-40px] md:mt-0 w-[88px] h-[88px] md:mr-8">
+              <img src={data.logo} alt="" className="max-w-[100%] h-auto " />
+          </div>
+          <div className="border-b border-[--Dark-Grayish-Cyan] md:border-0">
+              <p className="text-[--Primary-Dark-Cyan] font-bold inline mr-4">{data.company}</p>
+              {data.new ?  <span className="options boolean-span mr-4">New!</span> : null}
+              {data.featured ?  <span className="options featured-span">Featured</span> : null}
+              <p className="font-bold">{data.position}</p>
+              <span className="mr-4 details">{data.postedAt}</span>
+              <span className="mr-4 details">{data.contract}</span>
+              <span className="details">{data.location}</span>
+          </div>
         </div>
-        <div className="border-b border-black">
-            <p>{data.company}</p>
-            <span>{data.new}</span>
-            <span>{data.featured}</span>
-            <p>{data.position}</p>
-            <span>{data.postedAt}</span>
-            <span>{data.contract}</span>
-            <span>{data.location}</span>
-        </div>
-        <div>
-          <span>{data.role}</span>
-          <span>{data.level}</span>
-            {data.languages.map((lng,i)=>(
-              <span key={i}>{lng}</span>
+        <div className="flex flex-wrap gap-1 md:gap-x-4 mt-4">
+          <span className="filter-tablets" onClick={handleClick}>{data.role}</span>
+          <span className="filter-tablets" onClick={handleClick}>{data.level}</span>
+            {dataSet.map((item,i)=>(
+              <span key={i} className="filter-tablets" onClick={handleClick}>{item}</span>
             ))}
         </div>
     </div>
