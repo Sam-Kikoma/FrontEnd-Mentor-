798d72123eac7ceb97556d5805092ac6bb09f6f3
Sam Kikoma <61828265+Sam-Kikoma@users.noreply.github.com>
2024-07-16 21:30:05 +0200

    Delete Job-Listings directory

--- Job-Listings/app.js
+++
@@ -1,9 +0,0 @@
-async function getData() {
-    try {
-      const response = await fetch('data.json');
-      const data = await response.json();
-      console.log(data);
-    } catch (error) {
-      console.error(error);
-    }
-}
